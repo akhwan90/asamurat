145f808d43de3cdf9d863404f0699a6c48d43543
Nur Akhwan <akhwan90@gmail.com>
2015-04-09 21:58:57 +0700

    Update login.php
    Update di skrip action login, yang pada error di short open tag, dan .htaccess gak jalan. :)

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -72,7 +72,7 @@
 		</div>
 		
 		<div class="well" style="width: 400px; margin: 20px auto; border: solid 1px #d9d9d9; padding: 30px 20px; border-radius: 8px">
-		<form action="<?=base_URL()?>admin/do_login" method="post">
+		<form action="<?php echo base_URL(); ?>index.php/admin/do_login" method="post">
 		<legend>Login Admin</legend>	
 		<?=$this->session->flashdata("k")?>
 		<table align="center" style="margin-bottom: 0" class="table-form" width="90%">
