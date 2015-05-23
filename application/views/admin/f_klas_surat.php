0ff53d38b09118eb7d017456930249eb1a29f556
Nur Akhwan <akhwan90@gmail.com>
2015-05-23 09:03:36 +0700

    Update f_klas_surat.php

--- application/views/admin/f_klas_surat.php
+++ application/views/admin/f_klas_surat.php
@@ -24,7 +24,7 @@ if ($mode == "edt" || $mode == "act_edt") {
 
 <?php echo $this->session->flashdata("k_passwod");?>
 	
-<form action="<?=base_URL()?>admin/klas_surat/<?php echo $act; ?>" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+<form action="<?=base_URL()?>index.php/admin/klas_surat/<?php echo $act; ?>" method="post" accept-charset="utf-8" enctype="multipart/form-data">
 	<input type="hidden" name="idp" value="<?php echo $idp; ?>">
 	<table width="100%" class="table-form">
 	<tr><td width="20%">Nama</td><td><b><input type="text" name="nama" required value="<?php echo $nama; ?>" style="width: 400px" class="form-control"></b></td></tr>		
