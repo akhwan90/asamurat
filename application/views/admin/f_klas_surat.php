7f0c9bf8e76a7493f6bbe7e22fd6397f88c3825a
Nur Akhwan <akhwan90@gmail.com>
2015-05-29 17:32:14 +0700

    Update f_klas_surat.php

--- application/views/admin/f_klas_surat.php
+++ application/views/admin/f_klas_surat.php
@@ -16,22 +16,25 @@ if ($mode == "edt" || $mode == "act_edt") {
 <div class="navbar navbar-inverse">
 	<div class="container">
 		<div class="navbar-header">
-			<a class="navbar-brand" href="#">Klasifikasi Surat (PMA 44 Tahun 2010)
+			<a class="navbar-brand" href="#">Klasifikasi Surat (Peraturan Menteri Agama Nomor 44 Tahun 2010)
 </a>
 		</div>
 	</div><!-- /.container -->
 </div><!-- /.navbar -->
 
 <?php echo $this->session->flashdata("k_passwod");?>
-	
-<form action="<?=base_URL()?>index.php/admin/klas_surat/<?php echo $act; ?>" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+
+<div class="well">
+
+<form action="<?php echo base_URL(); ?>index.php/admin/klas_surat/<?php echo $act; ?>" method="post" accept-charset="utf-8" enctype="multipart/form-data">
 	<input type="hidden" name="idp" value="<?php echo $idp; ?>">
 	<table width="100%" class="table-form">
-	<tr><td width="20%">Nama</td><td><b><input type="text" name="nama" required value="<?php echo $nama; ?>" style="width: 400px" class="form-control"></b></td></tr>		
-	<tr><td width="20%">Uraian</td><td><b><textarea name="uraian" required style="width: 600px; height: 200px" class="form-control"><?php echo $uraian; ?></textarea></b></td></tr>		
+	<tr><td width="20%">Nama</td><td><b><input type="text" name="nama" required value="<?php echo $nama; ?>" style="width: 700px" class="form-control" autofocus></b></td></tr>		
+	<tr><td width="20%">Uraian</td><td><b><textarea name="uraian" required style="width: 700px; height: 100px" class="form-control"><?php echo $uraian; ?></textarea></b></td></tr>		
 	<tr><td colspan="2">
-	<br><button type="submit" class="btn btn-primary">Simpan</button>
-	<a href="<?=base_URL()?>admin/jenis_nilai" class="btn btn-success">Kembali</a>
+	<br><button type="submit" class="btn btn-primary"><i class="icon icon-ok icon-white"></i> Simpan</button>
+	<a href="<?php echo base_URL(); ?>index.php/admin/klas_surat" class="btn btn-success"><i class="icon icon-arrow-left icon-white"></i> Kembali</a>
 	</td></tr>
 	</table>
 </form>
+</div>
