ceff800a0ea8359bd7b2722fa5459b76051ee6ba
Nur Akhwan <akhwan90@gmail.com>
2015-05-29 17:34:53 +0700

    Update l_surat_disposisi.php

--- application/views/admin/l_surat_disposisi.php
+++ application/views/admin/l_surat_disposisi.php
@@ -9,16 +9,10 @@
 			</div>
 		<div class="navbar-collapse collapse navbar-inverse-collapse" style="margin-right: -20px">
 			<ul class="nav navbar-nav">
-				<li><a href="<?php echo base_URL(); ?>admin/surat_disposisi/<?php echo $this->uri->segment(3)?>/add" class="btn-info"><i class="icon-plus-sign icon-white"> </i> Tambah Data</a></li>
-				<li><a href="<?php echo base_URL(); ?>admin/surat_masuk" class="btn-info"><i class="icon-share icon-white"> </i> Kembali</a></li>
+				<li><a href="<?php echo base_URL(); ?>index.php/admin/surat_disposisi/<?php echo $this->uri->segment(3)?>/add" class="btn-info"><i class="icon-plus-sign icon-white"> </i> Tambah Data</a></li>
+				<li><a href="<?php echo base_URL(); ?>index.php/admin/surat_masuk" class="btn-info"><i class="icon-share icon-white"> </i> Kembali</a></li>
 			</ul>
 			
-			<ul class="nav navbar-nav navbar-right">
-				<form class="navbar-form navbar-left" method="post" action="<?=base_URL()?>admin/surat_disposisi/<?php echo $this->uri->segment(3)?>/cari">
-					<input type="text" class="form-control" name="q" style="width: 200px" placeholder="Kata kunci pencarian ..." required>
-					<button type="submit" class="btn btn-danger"><i class="icon-search icon-white"> </i> Cari</button>
-				</form>
-			</ul>
 		</div><!-- /.nav-collapse -->
 		</div><!-- /.container -->
 	</div><!-- /.navbar -->
@@ -28,18 +22,8 @@
 
 <?php echo $this->session->flashdata("k");?>
 	
-<!--	
-<div class="alert alert-dismissable alert-success">
-  <button type="button" class="close" data-dismiss="alert">x</button>
-  <strong>Well done!</strong> You successfully read <a href="http://bootswatch.com/amelia/#" class="alert-link">this important alert message</a>.
-</div>
-	
-<div class="alert alert-dismissable alert-danger">
-  <button type="button" class="close" data-dismiss="alert">x</button>
-  <strong>Oh snap!</strong> <a href="http://bootswatch.com/amelia/#" class="alert-link">Change a few things up</a> and try submitting again.
-</div>	
--->
-<b>Perihal Surat</b> : <i><?php echo $judul_surat; ?></i><br><br>
+<div class="alert alert-info">Perihal Surat</b> : <i><?php echo $judul_surat; ?></i></div>
+
 <table class="table table-bordered table-hover">
 	<thead>
 		<tr>
@@ -63,11 +47,11 @@
 			<td class="ctr"><?php echo $no;?></td>
 			<td><?php echo $b->kpd_yth; ?></td>
 			<td><?php echo $b->isi_disposisi; ?></td>
-			<td><?=$b->sifat."<br><i>Batas waktu s.d. ".tgl_jam_sql($b->batas_waktu)."</i>"?></td>
+			<td><?php echo $b->sifat."<br><i>Batas waktu s.d. ".tgl_jam_sql($b->batas_waktu)."</i>"?></td>
 			<td class="ctr">
 				<div class="btn-group">
-					<a href="<?=base_URL()?>admin/surat_disposisi/<?php echo $this->uri->segment(3)?>/edt/<?=$b->id?>" class="btn btn-success btn-sm"><i class="icon-edit icon-white"> </i> Edit</a>
-					<a href="<?=base_URL()?>admin/surat_disposisi/<?php echo $this->uri->segment(3)?>/del/<?=$b->id?>" class="btn btn-warning btn-sm" onclick="return confirm('Anda Yakin..?')">
+					<a href="<?php echo base_URL(); ?>index.php/admin/surat_disposisi/<?php echo $this->uri->segment(3)?>/edt/<?=$b->id?>" class="btn btn-success btn-sm"><i class="icon-edit icon-white"> </i> Edit</a>
+					<a href="<?php echo base_URL(); ?>index.php/admin/surat_disposisi/<?php echo $this->uri->segment(3)?>/del/<?=$b->id?>" class="btn btn-warning btn-sm" onclick="return confirm('Anda Yakin..?')">
 					<i class="icon-trash icon-white"> </i> Hapus</a>
 				</div>					
 			</td>
