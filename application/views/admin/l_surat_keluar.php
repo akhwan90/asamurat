5a40834892eb512289e1c99041f12c66979e76d6
Nur Akhwan <akhwan90@gmail.com>
2015-05-29 17:35:09 +0700

    Update l_surat_keluar.php

--- application/views/admin/l_surat_keluar.php
+++ application/views/admin/l_surat_keluar.php
@@ -9,11 +9,11 @@
 			</div>
 		<div class="navbar-collapse collapse navbar-inverse-collapse" style="margin-right: -20px">
 			<ul class="nav navbar-nav">
-				<li><a href="<?php echo base_URL(); ?>admin/surat_keluar/add" class="btn-info"><i class="icon-plus-sign icon-white"> </i> Tambah Data</a></li>
+				<li><a href="<?php echo base_URL(); ?>index.php/admin/surat_keluar/add" class="btn-info"><i class="icon-plus-sign icon-white"> </i> Tambah Data</a></li>
 			</ul>
 			
 			<ul class="nav navbar-nav navbar-right">
-				<form class="navbar-form navbar-left" method="post" action="<?=base_URL()?>admin/surat_keluar/cari">
+				<form class="navbar-form navbar-left" method="post" action="<?php echo base_URL(); ?>index.php/admin/surat_keluar/cari">
 					<input type="text" class="form-control" name="q" style="width: 200px" placeholder="Kata kunci pencarian ..." required>
 					<button type="submit" class="btn btn-danger"><i class="icon-search icon-white"> </i> Cari</button>
 				</form>
@@ -27,17 +27,6 @@
 
 <?php echo $this->session->flashdata("k");?>
 	
-<!--	
-<div class="alert alert-dismissable alert-success">
-  <button type="button" class="close" data-dismiss="alert">x</button>
-  <strong>Well done!</strong> You successfully read <a href="http://bootswatch.com/amelia/#" class="alert-link">this important alert message</a>.
-</div>
-	
-<div class="alert alert-dismissable alert-danger">
-  <button type="button" class="close" data-dismiss="alert">x</button>
-  <strong>Oh snap!</strong> <a href="http://bootswatch.com/amelia/#" class="alert-link">Change a few things up</a> and try submitting again.
-</div>	
--->
 
 <table class="table table-bordered table-hover">
 	<thead>
@@ -60,20 +49,20 @@
 		?>
 		<tr>
 			<td><?php echo $b->no_agenda."/".$b->kode;?></td>
-			<td><?=$b->isi_ringkas."<br><b>File : </b><i><a href='".base_URL()."upload/surat_keluar/".$b->file."' target='_blank'>".$b->file."</a>"?></td>
-			<td><?=$b->tujuan?></td>
-			<td><?=$b->no_surat."<br><i>".tgl_jam_sql($b->tgl_surat)."</i>"?></td>
+			<td><?php echo $b->isi_ringkas."<br><b>File : </b><i><a href='".base_URL()."upload/surat_keluar/".$b->file."' target='_blank'>".$b->file."</a>"?></td>
+			<td><?php echo $b->tujuan?></td>
+			<td><?php echo $b->no_surat."<br><i>".tgl_jam_sql($b->tgl_surat)."</i>"?></td>
 			<td class="ctr">
 				<?php  
 				if ($b->pengolah == $this->session->userdata('admin_id')) {
 				?>
 				<div class="btn-group">
-					<a href="<?=base_URL()?>admin/surat_keluar/edt/<?=$b->id?>" class="btn btn-success btn-sm"><i class="icon-edit icon-white"> </i> Edit</a>
-					<a href="<?=base_URL()?>admin/surat_keluar/del/<?=$b->id?>" class="btn btn-warning btn-sm" onclick="return confirm('Anda Yakin..?')">
+					<a href="<?php echo base_URL()?>index.php/admin/surat_keluar/edt/<?php echo $b->id?>" class="btn btn-success btn-sm"><i class="icon-edit icon-white"> </i> Edit</a>
+					<a href="<?php echo base_URL()?>index.php/admin/surat_keluar/del/<?php echo $b->id?>" class="btn btn-warning btn-sm" onclick="return confirm('Anda Yakin..?')">
 					<i class="icon-trash icon-white"> </i> Hapus</a>
 				</div>	
 				<?php } else { ?>
-				No Action
+				<div class="label label-danger">No Action</div>
 				<?php } ?>
 			</td>
 		</tr>
