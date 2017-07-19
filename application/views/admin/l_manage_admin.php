c96ff5efd822ebed71de9982efcd97f4ac127ae3
Nur Akhwan <akhwan90@gmail.com>
2017-07-19 13:10:21 +0700

    Update versi 1.2

--- application/views/admin/l_manage_admin.php
+++ application/views/admin/l_manage_admin.php
@@ -1,29 +1,21 @@
 <div class="clearfix">
-<div class="row">
-  <div class="col-lg-12">
-	
-	<div class="navbar navbar-inverse">
-		<div class="container">
-			<div class="navbar-header">
-				<span class="navbar-brand">Manage Admin</span>
-			</div>
-		<div class="navbar-collapse collapse navbar-inverse-collapse" style="margin-right: -20px">
-			<ul class="nav navbar-nav">
-				<li><a href="<?php echo base_URL(); ?>index.php/admin/manage_admin/add" class="btn-info"><i class="icon-plus-sign icon-white"> </i> Tambah Data</a></li>
-			</ul>
-			
-			<ul class="nav navbar-nav navbar-right">
-				<form class="navbar-form navbar-left" method="post" action="<?php echo base_URL(); ?>admin/manage_admin/cari">
-					<input type="text" class="form-control" name="q" style="width: 200px" placeholder="Kata kunci pencarian ..." required>
-					<button type="submit" class="btn btn-danger"><i class="icon-search icon-white"> </i> Cari</button>
-				</form>
-			</ul>
-		</div><!-- /.nav-collapse -->
-		</div><!-- /.container -->
-	</div><!-- /.navbar -->
 
-  </div>
+<div class="panel panel-info">
+	<div class="panel-heading" style="overflow: auto">
+		<div class="col-md-3"><h3 style="margin-top: 5px">Manage Admin</h3></div>
+		<div class="col-md-2">
+			<a href="<?php echo base_URL(); ?>index.php/admin/manage_admin/add" class="btn btn-info"><i class="icon-plus-sign icon-white"> </i> Tambah Data</a>
+		</div>
+		<div class="col-md-3"></div>
+		<div class="col-md-4">
+			<form class="navbar-form navbar-left" method="post" action="<?php echo base_URL(); ?>index.php/admin/manage_admin/cari" style="margin-top: 0px">
+				<input type="text" class="form-control" name="q" style="width: 200px" placeholder="Kata kunci pencarian ..." required>
+				<button type="submit" class="btn btn-danger"><i class="icon-search icon-white"> </i> Cari</button>
+			</form>
+		</div>
+	</div>
 </div>
+
 
 <?php echo $this->session->flashdata("k");?>
 
@@ -53,8 +45,7 @@
 			<td><?php echo $b->level?></td>
 			<td class="ctr">
 				<div class="btn-group">
-					<a href="<?php echo base_URL(); ?>index.php/admin/manage_admin/edt/<?php echo $b->id; ?>" class="btn btn-success btn-sm" title="Edit Data"><i class="icon-edit icon-white"> </i> Edt</a>
-					<!--<a href="<?php echo base_URL(); ?>admin/manage_admin/del/<?php echo $b->id?>" class="btn btn-warning btn-sm" title="Hapus Data" onclick="return confirm('Anda Yakin..?')"><i class="icon-trash icon-remove">  </i> Del</a>-->			
+					<a href="<?php echo base_URL(); ?>index.php/admin/manage_admin/edt/<?php echo $b->id; ?>" class="btn btn-success btn-sm" title="Edit Data"><i class="icon-edit icon-white"> </i> Edt</a><a href="<?php echo base_URL(); ?>index.php/admin/manage_admin/del/<?php echo $b->id?>" class="btn btn-warning btn-sm" title="Hapus Data" onclick="return confirm('Anda Yakin..?')"><i class="icon-trash icon-remove">  </i> Del</a>		
 				</div>					
 			</td>
 		</tr>
