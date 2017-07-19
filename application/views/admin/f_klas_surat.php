8c02ac67bb5a2b5375c20206d9e305a17ae9176a
Nur Akhwan <akhwan90@gmail.com>
2017-07-19 13:07:05 +0700

    Update versi 1.2

--- application/views/admin/f_klas_surat.php
+++ application/views/admin/f_klas_surat.php
@@ -4,23 +4,20 @@ $mode		= $this->uri->segment(3);
 if ($mode == "edt" || $mode == "act_edt") {
 	$act		= "act_edt";
 	$idp		= $datpil->id;
+	$kode		= $datpil->kode;	
 	$nama		= $datpil->nama;	
 	$uraian		= $datpil->uraian;	
 } else {
 	$act		= "act_add";
 	$idp		= "";
+	$kode		= "";
 	$nama		= "";
 	$uraian		= "";
 }
 ?>
-<div class="navbar navbar-inverse">
-	<div class="container">
-		<div class="navbar-header">
-			<a class="navbar-brand" href="#">Klasifikasi Surat (Peraturan Menteri Agama Nomor 44 Tahun 2010)
-</a>
-		</div>
-	</div><!-- /.container -->
-</div><!-- /.navbar -->
+<div class="panel panel-info">
+	<div class="panel-heading"><h3 style="margin-top: 5px">Klasifikasi Surat</h3></div>
+</div>
 
 <?php echo $this->session->flashdata("k_passwod");?>
 
@@ -29,7 +26,8 @@ if ($mode == "edt" || $mode == "act_edt") {
 <form action="<?php echo base_URL(); ?>index.php/admin/klas_surat/<?php echo $act; ?>" method="post" accept-charset="utf-8" enctype="multipart/form-data">
 	<input type="hidden" name="idp" value="<?php echo $idp; ?>">
 	<table width="100%" class="table-form">
-	<tr><td width="20%">Nama</td><td><b><input type="text" name="nama" required value="<?php echo $nama; ?>" style="width: 700px" class="form-control" autofocus></b></td></tr>		
+	<tr><td width="20%">Kode</td><td><b><input type="text" name="kode" required value="<?php echo $kode; ?>" style="width: 700px" class="form-control" autofocus></b></td></tr>		
+	<tr><td width="20%">Nama</td><td><b><input type="text" name="nama" required value="<?php echo $nama; ?>" style="width: 700px" class="form-control"></b></td></tr>		
 	<tr><td width="20%">Uraian</td><td><b><textarea name="uraian" required style="width: 700px; height: 100px" class="form-control"><?php echo $uraian; ?></textarea></b></td></tr>		
 	<tr><td colspan="2">
 	<br><button type="submit" class="btn btn-primary"><i class="icon icon-ok icon-white"></i> Simpan</button>
