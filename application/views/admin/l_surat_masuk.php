ee13adb1fd4f471b6a62925c5381072865b1b329
Nur Akhwan <akhwan90@gmail.com>
2017-07-19 13:12:09 +0700

    Update versi 1.2

--- application/views/admin/l_surat_masuk.php
+++ application/views/admin/l_surat_masuk.php
@@ -1,43 +1,22 @@
 <div class="clearfix">
-<div class="row">
-  <div class="col-lg-12">
-	
-	<div class="navbar navbar-inverse">
-		<div class="container">
-			<div class="navbar-header">
-				<a class="navbar-brand" href="#">Surat Masuk</a>
-			</div>
-		<div class="navbar-collapse collapse navbar-inverse-collapse" style="margin-right: -20px">
-			<ul class="nav navbar-nav">
-				<li><a href="<?php echo base_URL(); ?>index.php/admin/surat_masuk/add" class="btn-info"><i class="icon-plus-sign icon-white"> </i> Tambah Data</a></li>
-			</ul>
-			
-			<ul class="nav navbar-nav navbar-right">
-				<form class="navbar-form navbar-left" method="post" action="<?php echo base_URL(); ?>index.php/admin/surat_masuk/cari">
-					<input type="text" class="form-control" name="q" style="width: 200px" placeholder="Kata kunci pencarian ..." required>
-					<button type="submit" class="btn btn-danger"><i class="icon-search icon-white"> </i> Cari</button>
-				</form>
-			</ul>
-		</div><!-- /.nav-collapse -->
-		</div><!-- /.container -->
-	</div><!-- /.navbar -->
 
-  </div>
+<div class="panel panel-info">
+	<div class="panel-heading" style="overflow: auto">
+		<div class="col-md-2"><h3 style="margin-top: 5px">Surat Masuk</h3></div>
+		<div class="col-md-2">
+			<a href="<?php echo base_URL(); ?>index.php/admin/surat_masuk/add" class="btn btn-info"><i class="icon-plus-sign icon-white"> </i> Tambah Data</a>
+		</div>
+		<div class="col-md-4"></div>
+		<div class="col-md-4">
+			<form class="navbar-form navbar-left" method="post" action="<?php echo base_URL(); ?>index.php/admin/surat_masuk/cari" style="margin-top: 0px">
+				<input type="text" class="form-control" name="q" style="width: 200px" placeholder="Kata kunci pencarian ..." required>
+				<button type="submit" class="btn btn-danger"><i class="icon-search icon-white"> </i> Cari</button>
+			</form>
+		</div>
+	</div>
 </div>
 
 <?php echo $this->session->flashdata("k");?>
-	
-<!--	
-<div class="alert alert-dismissable alert-success">
-  <button type="button" class="close" data-dismiss="alert">x</button>
-  <strong>Well done!</strong> You successfully read <a href="http://bootswatch.com/amelia/#" class="alert-link">this important alert message</a>.
-</div>
-	
-<div class="alert alert-dismissable alert-danger">
-  <button type="button" class="close" data-dismiss="alert">x</button>
-  <strong>Oh snap!</strong> <a href="http://bootswatch.com/amelia/#" class="alert-link">Change a few things up</a> and try submitting again.
-</div>	
--->
 
 <table class="table table-bordered table-hover">
 	<thead>
