2241305f6d040f8da305b599200e4dee03958c40
Nur Akhwan <akhwan90@gmail.com>
2017-07-19 13:09:54 +0700

    Update versi 1.2

--- application/views/admin/l_klas_surat.php
+++ application/views/admin/l_klas_surat.php
@@ -1,47 +1,35 @@
 <div class="clearfix">
-<div class="row">
-  <div class="col-lg-12">
-	
-	<div class="navbar navbar-inverse">
-		<div class="container">
-			<div class="navbar-header">
-				<a class="navbar-brand" href="#">Klasifikasi Surat (Peraturan Menteri Agama Nomor 44 Tahun 2010)</a>
-			</div>
-		<div class="navbar-collapse collapse navbar-inverse-collapse" style="margin-right: -20px">
-			<ul class="nav navbar-nav navbar-right">
-				<form class="navbar-form navbar-left" method="post" action="<?php echo base_URL(); ?>index.php/admin/klas_surat/cari">
-					<input type="text" class="form-control" name="q" style="width: 200px" placeholder="Kata kunci pencarian ..." required>
-					<button type="submit" class="btn btn-danger"><i class="icon-search icon-white"> </i> Cari</button>
-				</form>
-			</ul>
-		</div><!-- /.nav-collapse -->
-		</div><!-- /.container -->
-	</div><!-- /.navbar -->
 
-  </div>
+<div class="panel panel-info">
+	<div class="panel-heading" style="overflow: auto">
+		<div class="col-md-3"><h3 style="margin-top: 5px">Klasifikasi Surat</h3></div>
+		<?php 
+		if ($this->session->userdata('admin_level') == "Super Admin") {
+		?>
+		<div class="col-md-2">
+			<a href="<?php echo base_URL(); ?>index.php/admin/klas_surat/add" class="btn btn-info"><i class="icon-plus-sign icon-white"> </i> Tambah Data</a>
+		</div>
+		<?php } ?>
+		<div class="col-md-3"></div>
+		<div class="col-md-4">
+			<form class="navbar-form navbar-left" method="post" action="<?php echo base_URL(); ?>index.php/admin/klas_surat/cari" style="margin-top: 0px">
+				<input type="text" class="form-control" name="q" style="width: 200px" placeholder="Kata kunci pencarian ..." required>
+				<button type="submit" class="btn btn-danger"><i class="icon-search icon-white"> </i> Cari</button>
+			</form>
+		</div>
+	</div>
 </div>
+
 
 <?php echo $this->session->flashdata("k");?>
-	
-<!--	
-<div class="alert alert-dismissable alert-success">
-  <button type="button" class="close" data-dismiss="alert">x</button>
-  <strong>Well done!</strong> You successfully read <a href="http://bootswatch.com/amelia/#" class="alert-link">this important alert message</a>.
-</div>
-	
-<div class="alert alert-dismissable alert-danger">
-  <button type="button" class="close" data-dismiss="alert">x</button>
-  <strong>Oh snap!</strong> <a href="http://bootswatch.com/amelia/#" class="alert-link">Change a few things up</a> and try submitting again.
-</div>	
--->
 
 <table class="table table-bordered table-hover">
 	<thead>
 		<tr>
 			<th width="10%">Kode</th>
 			<th width="20%">Nama</th>
-			<th width="60%">Bobot</th>
-			<th width="10%">Aksi</th>
+			<th width="50%">Bobot</th>
+			<th width="20%">Aksi</th>
 		</tr>
 	</thead>
 	
@@ -64,6 +52,8 @@
 			<td class="ctr">
 				<div class="btn-group">
 					<a href="<?php echo base_URL(); ?>index.php/admin/klas_surat/edt/<?php echo $b->id; ?>" class="btn btn-success btn-sm"><i class="icon-edit icon-white"> </i> Edit</a>
+					<a href="<?php echo base_URL()?>index.php/admin/klas_surat/del/<?php echo $b->id?>" class="btn btn-warning btn-sm" onclick="return confirm('Anda Yakin..?')">
+					<i class="icon-trash icon-white"> </i> Hapus</a>
 				</div>					
 			</td>
 			<?php 
